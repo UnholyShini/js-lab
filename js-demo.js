3bed057b648f2fd27e20dd3e5e3d22a9cd45b00c
UnholyShini <130506712+UnholyShini@users.noreply.github.com>
2023-04-11 15:15:02 -0700

    Update js-demo.js

--- js-demo.js
+++ js-demo.js
@@ -12,11 +12,11 @@ runClock();
 setInterval("runClock()" , 1000);
 
 function runClock(){
-var thisDay=new Date();
-var thisDate=thisDay.toLocaleDateString();
-var thisDayNum=thisDay.getDay();
-var thisWeekday=getWeekday(thisDayNum);
-var thisTime=thisDay.toLocaleTimeString();
+var thisDay = new Date();
+var thisDate = thisDay.toLocaleDateString();
+var thisDayNum = thisDay.getDay();
+var thisWeekday = getWeekday(thisDayNum);
+var thisTime = thisDay.toLocaleTimeString();
 
 document.getElementById("date").textContent=thisDate;
 document.getElementById("wday").textContent=thisWeekday;
