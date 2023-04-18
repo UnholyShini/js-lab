002a7d3ff1a909f7a36329822af1f8d7b4286cfd
UnholyShini <130506712+UnholyShini@users.noreply.github.com>
2023-04-18 15:57:04 -0700

    Add files via upload

--- js-demo.js
+++ js-demo.js
@@ -1,15 +1,5 @@
-/*
-   JS Activity Clock
-   Name: Dillon Micheli
-   Date:   
-
-   function getWeekday(dayNum)
-      Returns the text of the day of the week where dayNum
-      is the number of the week from 0 (Sunday) to 6 (Saturday)
-*/
-
-runClock();
-setInterval("runClock()" , 1000);
+﻿runClock();
+setInterval("runClock()",1000);
 
 function runClock(){
 var thisDay = new Date();
@@ -25,5 +15,5 @@ document.getElementById("time").textContent= thisTime;
 
 function getWeekday(dayNum){
    var wDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
-return wDays[dayNum];
+   return wDays[dayNum];
 }
